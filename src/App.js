c42aa855b4b5ccf259a1f7999084a8da65971d72
Adrian Américo <1fnadrian1@gmail.com>
2022-10-18 10:53:28 -0300

    change message

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      test
+    </div>
+  );
 };
 
 export default App;
